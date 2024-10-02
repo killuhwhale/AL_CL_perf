103d1fae387c7d2f32f2fc3bc0bbda6685415bff
killuh <20297127+killuhwhale@users.noreply.github.com>
2024-10-02 12:32:14 -0700

    open inspector retry loop

--- src/playstore/config.py
+++ src/playstore/config.py
@@ -12,7 +12,7 @@ def get_save_coords():
     if mac_address == "17:29:bf:99:23:6a":
         # Personal
         SAVE_BTN_COORDS = (2013, 354,)
-    elif mac_address == "7f:7e:5c:29:53:31":
+    elif mac_address in ["7f:7e:5c:29:53:31", "97:24:3f:c7:ac:2f"]:
         # AMD Linux Laptop RED
         SAVE_BTN_COORDS = (1867, 300,)
     return SAVE_BTN_COORDS
